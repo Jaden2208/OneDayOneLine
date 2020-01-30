c3526c59effe82cfa68f8de87c3c948a70b6e8de
ryu22 <ryu2208@naver.com>
2020-01-31 01:05:23 +0900

    PostActivity back 버튼 활성화

--- app/src/main/java/com/whalez/onedayoneline/ui/PostActivity.kt
+++ app/src/main/java/com/whalez/onedayoneline/ui/PostActivity.kt
@@ -3,11 +3,15 @@ package com.whalez.onedayoneline.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.whalez.onedayoneline.R
+import kotlinx.android.synthetic.main.activity_post.*
 
 class PostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        // back 버튼 클릭
+        btn_back.setOnClickListener { finish() }
     }
 }
